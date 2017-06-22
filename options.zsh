b6a27f8772955da4730f4430e2e9c12f480dc95c
Lars Eidam <lars.eidam@googlemail.com>
2017-06-22 20:53:37 +0200

    repair history search

--- options.zsh
+++ options.zsh
@@ -19,8 +19,12 @@ bindkey "^[[H" .backward-word
 bindkey "^[[F" .forward-word
 
 # arrow up/down searches in history if line is already started
-bindkey '^[[A' up-line-or-search
-bindkey '^[[B' down-line-or-search
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "$terminfo[kcuu1]" up-line-or-beginning-search # Up
+bindkey "$terminfo[kcud1]" down-line-or-beginning-search # Down
 
 # History Settings (big history for use with many open shells and no dups)
 # Different History files for root and standard user
